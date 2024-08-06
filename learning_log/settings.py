6b7e2938fb153fc78cdca97ad3eaac10f74ee607
chichi88359 <koutarou_bumper@icloud.com>
2024-08-07 00:33:37 +0900

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -16,6 +16,9 @@ import environ
 from decouple import config
 import dj_database_url
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
